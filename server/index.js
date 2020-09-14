cb7ade8946ee43fb9f7b7d4f5fd13c74b89f1f1c
geeky1Coder <yuvrajmann282@gmail.com>
2020-09-14 14:03:43 +0530

    image uploaded

--- server/index.js
+++ server/index.js
@@ -4,29 +4,43 @@ const hostName = "localhost";
 const portNumber = "5000";
 var server = http.createServer(app);
 const io = require("socket.io")(server);
-// const addUser = require("./users");
+const Users = require("./users");
 
 io.on("connection", (socket) => {
-  console.log("user connected");
   socket.on("join", ({ userName, roomId }) => {
     socket.join(roomId);
-    // addUser(userName, roomId);
-    console.log(`${userName} joined the room ${roomId}`);
-    socket.emit("newUser", {
-      userName: userName,
-      roomId: roomId,
-      msg: `${userName} you are connected to the room ${roomId}`,
+    Users.addUser(userName, roomId, (err, push_user) => {
+      if (!err) {
+        push_user.pushUser();
+        console.log(`${userName} joined the room ${roomId}`);
+        socket.emit("newUser", {
+          userName: userName,
+          roomId: roomId,
+          msg: `${userName} you are connected to the room ${roomId}`,
+        });
+        socket.to(roomId).broadcast.emit("newUser", {
+          userName: userName,
+          roomId: roomId,
+          msg: `${userName} joined the room`,
+        });
+        console.log(Users.getUsers());
+      } else {
+        console.log(`${userName} can't join the room ${roomId} ${err}`);
+      }
     });
-    socket.to(roomId).broadcast.emit("newUser", {
-      userName: userName,
-      roomId: roomId,
-      msg: `${userName} joined the room`,
-    });
+
     socket.on("disconnect", () => {
-      console.log(`${userName} disconnected from room ${roomId}`);
-      socket.to(roomId).broadcast.emit("chat-message", {
-        userName: "admin",
-        chatMessage: `${userName} left the room`,
+      Users.removeUser(userName, roomId, (err, remove_user) => {
+        if (err) {
+          console.log(`${err}`);
+        } else {
+          remove_user.remove_user();
+          console.log(`${userName} disconnected from room ${roomId}`);
+          socket.to(roomId).broadcast.emit("chat-message", {
+            userName: "admin",
+            chatMessage: `${userName} left the room`,
+          });
+        }
       });
     });
   });
--- server/users.js
+++ server/users.js
@@ -3,8 +3,8 @@ const Users = [];
 const addUser = (userName, roomId, callback) => {
   if (
     Users.filter((user) => {
-      user === userName && user.roomId === roomId;
-    }) > 0
+      return user.userName === userName && user.roomId === roomId;
+    }).length > 0
   ) {
     callback(
       new Error("User with this username already exists ,choose another"),
@@ -17,7 +17,34 @@ const addUser = (userName, roomId, callback) => {
       },
     });
   }
-  Users.push({ userName, roomId });
 };
-const removeUser = (userName, roomId) => {};
-module.exports = addUser;
+const getUsers = () => {
+  return Users;
+};
+
+const removeUser = (userName, roomId, callback) => {
+  if (
+    Users.filter((user) => {
+      return user.userName === userName && user.roomId === roomId;
+    }).length == 0
+  ) {
+    callback(
+      new Error("User with this username donot exists,choose another"),
+      null
+    );
+  } else {
+    callback(null, {
+      remove_user: () => {
+        const conditioner = (user) => {
+          return user.userName === userName && user.roomId === user.roomId;
+        };
+        let index = Users.findIndex(conditioner);
+        console.log(index);
+        if (index > -1) {
+          Users.splice(index, 1);
+        }
+      },
+    });
+  }
+};
+module.exports = { addUser, getUsers, removeUser };
